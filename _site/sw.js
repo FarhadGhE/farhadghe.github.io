60c145b90868640058772305cc88979424135012
Farhad Gholami <farhad.gh.e@gmail.com>
2021-07-30 20:20:08 +0430

    rebuild

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210730.2013';
+const cacheName = 'chirpy-20210730.2019';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
